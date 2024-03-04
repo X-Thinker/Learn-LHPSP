34afb93e56b294172cf274fae542efa176077171
Think <Think@163.com>
2024-03-04 23:15:14 +0800

    Chapter5.4

---
+++ Chapter5/CodeList-5-1/code-5-1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//判断机器字节序
+void byteorder()
+{
+    union 
+    {
+        //short和char[]共享空间
+        //char[0]为低地址端，char[1]为高地址端
+        short value;
+        char union_byte[sizeof(short)];
+    }test;
+    //整数高位为 1,低位为 2
+    test.value = 0x0102;
+
+    //整数高位放低地址端：大端字节序/网络字节序
+    //整数高位放高地址端：小端字节序/主机字节序
+    if((test.union_byte[0] == 1) && (test.union_byte[1] == 2))
+        printf("big endian\n");
+    else if((test.union_byte[0] == 2) && (test.union_byte[1] == 1))
+        printf("little endian\n");
+    else 
+        printf("unknown...\n");
+}
+
+int main(void)
+{
+    byteorder();
+    //输出结果为小端字节序
+    return 0;
+}
---
+++ Chapter5/CodeList-5-2/code-5-2.c
@@ -0,0 +1,17 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+
+int main(void)
+{
+    struct in_addr addr1;
+    addr1.s_addr = 1234;
+    struct in_addr addr2;
+    addr2.s_addr = 101947160;
+    char *szValue1 = inet_ntoa(addr1);
+    char *szValue2 = inet_ntoa(addr2);
+    //输出结果显示两者地址相同
+    //揭示了 inet_ntoa 函数的不可重入性
+    printf("address 1:%s\n", szValue1);
+    printf("address 2:%s\n", szValue2);
+    return 0;
+}
