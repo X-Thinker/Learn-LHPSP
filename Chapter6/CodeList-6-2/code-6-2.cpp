93f4b03209dbc47c551de647b12e5f1a78a38c53
Think <xqk2775343@outlook.com>
2024-03-18 12:09:20 +0800

    Chapter6.5

--- Chapter6/CodeList-6-2/code-6-2.cpp
+++ Chapter6/CodeList-6-2/code-6-2.cpp
@@ -46,6 +46,7 @@ int main(int argc, char *argv[])
     
     struct sockaddr_in client;
     socklen_t client_addrlength = sizeof(client);
+
     int connfd = accept(sock, (struct sockaddr*)&client, &client_addrlength);
     if(connfd < 0)
         printf("errno is %d\n", errno);
@@ -62,7 +63,7 @@ int main(int argc, char *argv[])
         bool valid = true;
         /*缓冲区header_buf目前已经使用了多少字节的空间*/
         int len = 0;
-
+        
         if(stat(file_name, &file_stat) < 0)/*目标文件不存在*/
             valid = false;
         else 
@@ -78,42 +79,41 @@ int main(int argc, char *argv[])
                 memset(file_buf, '\0', file_stat.st_size + 1);
                 if(read(fd, file_buf, file_stat.st_size) < 0)
                     valid = false;
-                else 
-                    valid = false;
-
-                /*如果目标文件有效，则发送正常的HTTP应答*/
-                if(valid)
-                {
-                    /*下面这部分内容将HTTP应答的状态行，“Content-Length”头部字段和一个空行依次加入header_buf中*/
-                    ret = snprintf(header_buf, BUFFER_SIZE - 1, "%s%s\r\n", "HTTP/1.1", status_line[0]);
-                    len += ret;
-                    ret = snprintf(header_buf + len, BUFFER_SIZE - 1 - len, "Content-Length:%d\r\n", file_stat.st_size);
-                    len += ret;
-                    ret = snprintf(header_buf + len, BUFFER_SIZE - 1 - len, "%s", "\r\n");
-
-                    /*利用writev将header_buf和file_buf的内容一并写出*/ 
-                    struct iovec iv[2];
-                    iv[0].iov_base = header_buf;
-                    iv[0].iov_len = strlen(header_buf);
-                    iv[1].iov_base = file_buf;
-                    iv[1].iov_len = file_stat.st_size;
-                    ret = writev(connfd, iv, 2);
-                }
-                else /*如果目标文件无效，则通知客户端服务器发生了“内部错误”*/
-                {
-                    ret = snprintf(header_buf, BUFFER_SIZE - 1, "%s%s\r\n", "HTTP/1.1", status_line[1]);
-                    len += ret;
-                    ret = snprintf(header_buf + len, BUFFER_SIZE - 1 - len, "%s", "\r\n");
-                    send(connfd, header_buf, strlen(header_buf), 0);
-                }
-
-                close(connfd);
-                delete []file_buf;
             }
-
-            close(sock);
-            return 0;
+            else 
+                valid = false;
         }
-    }
 
+        /*如果目标文件有效，则发送正常的HTTP应答*/
+        if(valid)
+        {
+            /*下面这部分内容将HTTP应答的状态行，“Content-Length”头部字段和一个空行依次加入header_buf中*/
+            ret = snprintf(header_buf, BUFFER_SIZE - 1, "%s %s\r\n", "HTTP/1.1", status_line[0]);
+            len += ret;
+            ret = snprintf(header_buf + len, BUFFER_SIZE - 1 - len, "Content-Length:%d\r\n", file_stat.st_size);
+            len += ret;
+            ret = snprintf(header_buf + len, BUFFER_SIZE - 1 - len, "%s", "\r\n");
+
+            /*利用writev将header_buf和file_buf的内容一并写出*/ 
+            struct iovec iv[2];
+            iv[0].iov_base = header_buf;
+            iv[0].iov_len = strlen(header_buf);
+            iv[1].iov_base = file_buf;
+            iv[1].iov_len = file_stat.st_size;
+            ret = writev(connfd, iv, 2);
+            
+        }
+        else /*如果目标文件无效，则通知客户端服务器发生了“内部错误”*/
+        {
+            ret = snprintf(header_buf, BUFFER_SIZE - 1, "%s%s\r\n", "HTTP/1.1", status_line[1]);
+            len += ret;
+            ret = snprintf(header_buf + len, BUFFER_SIZE - 1 - len, "%s", "\r\n");                    send(connfd, header_buf, strlen(header_buf), 0);
+        }
+        
+        close(connfd);
+        delete []file_buf;
+    }
+    
+    close(sock);
+    return 0;
 }
